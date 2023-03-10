e7b4b0fc9f75a75611f62cef59d772f1db4b4add
sunil-2000 <sunilsabnis10@gmail.com>
2023-03-10 13:51:11 -0500

    to gif implemented

--- src/diffusion.py
+++ src/diffusion.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+from PIL import Image
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 
@@ -27,3 +29,34 @@ class Diffusion:
         images = self.pipe(prompts).images
         for i, image in enumerate(images):
             image.save(f"{output_dir}/{i}_{img_name}.png")
+
+    def text_to_gif(self, prompts, img_name="diffusion_img", output_dir="./"):
+        """
+        return image collection (GIF) of latent states from generative process
+        """
+        # single image as of now
+        latent_states = []
+
+        def callback_fn(i, t, latent):
+            latent_states.append(latent)
+
+        self.pipe(prompts, callback=callback_fn)
+        self._latent_lst_to_gif(latent_states, img_name, output_dir=output_dir)
+        return latent_states
+
+    def _latent_lst_to_gif(self, latent_states, img_name, output_dir):
+        """
+        convert latent state list to GIF
+        """
+        img_latent_states = [
+            np.array(latent_state * 255, dtype="uint8").squeeze()
+            for latent_state in latent_states
+        ]
+        images = [Image.fromarray(frame) for frame in img_latent_states]
+        images[0].save(
+            f"{output_dir}{img_name}.gif",
+            save_all=True,
+            append_images=images[1:],
+            duration=50,
+            loop=0,
+        )
